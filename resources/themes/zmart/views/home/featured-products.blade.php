f199a84c4cf92e8464617eda19cd024466e01dd4
Priyanka Vaishnav <priyanka.hashchains@gmail.com>
2021-01-22 20:41:06 +0530

    Featured Product Changes

--- resources/themes/zmart/views/home/featured-products.blade.php
+++ resources/themes/zmart/views/home/featured-products.blade.php
@@ -1,104 +1,21 @@
-@php
-    $count = $velocityMetaData ? $velocityMetaData->featured_product_count : 10;
-    $direction = core()->getCurrentLocale()->direction == 'rtl' ? 'rtl' : 'ltr';
-@endphp
+@if (app('Webkul\Product\Repositories\ProductRepository')->getFeaturedProducts()->count())
+    <section class="featured-products">
 
-<featured-products></featured-products>
+        <div class="featured-heading">
+            {{ __('shop::app.home.featured-products') }}<br/>
 
-@push('scripts')
-    <script type="text/x-template" id="featured-products-template">
-        <div class="container-fluid featured-products">
-            <shimmer-component v-if="isLoading && !isMobileView"></shimmer-component>
+            <span class="featured-seperator" style="color:lightgrey;">_____</span>
+        </div>
+        <div class="featured-grid product-grid-4">
+            <?php //echo"<pre>"; print_r(app('Webkul\Product\Repositories\ProductRepository')->getFeaturedProducts());exit(); ?>
 
-            <template v-else-if="featuredProducts.length > 0">
-                <card-list-header heading="{{ __('shop::app.home.featured-products') }}">
-                </card-list-header>
+            @foreach (app('Webkul\Product\Repositories\ProductRepository')->getFeaturedProducts() as $productFlat)
 
-                <div class="carousel-products vc-full-screen {{ $direction }}" v-if="!isMobileView">
-                    <carousel-component
-                        slides-per-page="6"
-                        navigation-enabled="hide"
-                        pagination-enabled="hide"
-                        id="fearured-products-carousel"
-                        locale-direction="{{ $direction }}"
-                        :autoplay="false"
-                        :slides-count="featuredProducts.length">
+                @include ('shop::products.newproduct.new-product-listing', ['product' => $productFlat])
 
-                        <slide
-                            :key="index"
-                            :slot="`slide-${index}`"
-                            v-for="(product, index) in featuredProducts">
-                            <product-card
-                                :list="list"
-                                :product="product">
-                            </product-card>
-                        </slide>
-                    </carousel-component>
-                </div>
-
-                <div class="carousel-products vc-small-screen {{ $direction }}" v-else>
-                    <carousel-component
-                        slides-per-page="2"
-                        navigation-enabled="hide"
-                        pagination-enabled="hide"
-                        id="fearured-products-carousel"
-                        locale-direction="{{ $direction }}"
-                        :slides-count="featuredProducts.length">
-
-                        <slide
-                            :key="index"
-                            :slot="`slide-${index}`"
-                            v-for="(product, index) in featuredProducts">
-                            <product-card
-                                :list="list"
-                                :product="product">
-                            </product-card>
-                        </slide>
-                    </carousel-component>
-                </div>
-            </template>
+            @endforeach
 
         </div>
-    </script>
 
-    <script type="text/javascript">
-        (() => {
-            Vue.component('featured-products', {
-                'template': '#featured-products-template',
-                data: function () {
-                    return {
-                        'list': false,
-                        'isLoading': true,
-                        'featuredProducts': [],
-                        'isMobileView': this.$root.isMobile(),
-                    }
-                },
-
-                mounted: function () {
-                    this.getFeaturedProducts();
-                },
-
-                methods: {
-                    'getFeaturedProducts': function () {
-                        this.$http.get(`${this.baseUrl}/category-details?category-slug=featured-products&count={{ $count }}`)
-                        .then(response => {
-                            var count = '{{$count}}';
-                            if (response.data.status && count != 0 )
-                            {
-                                this.featuredProducts = response.data.products;
-                            }else{
-                                this.featuredProducts = 0;   
-                            }
-
-                            this.isLoading = false;
-                        })
-                        .catch(error => {
-                            this.isLoading = false;
-                            console.log(this.__('error.something_went_wrong'));
-                        })
-                    }
-                }
-            })
-        })()
-    </script>
-@endpush
+    </section>
+@endif
