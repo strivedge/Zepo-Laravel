fce36a3127e1c36a14ad2a0f473d4c2c4a0eabf0
Kishan Parikh <kishan.strivedge@gmail.com>
2021-03-10 12:32:41 +0530

    Blog Design,Offers Design,Responsive Designs

--- resources/themes/zmart/views/home/blog.blade.php
+++ resources/themes/zmart/views/home/blog.blade.php
@@ -10,26 +10,48 @@
 @endsection
 
 @section('content-wrapper')
+	<section class="blog">
+			<div class="page-header">
+			    <div class="page-title">
+			        <h2>
+			            {{ __('blog::app.blogs.title') }}
+			        </h2>
+			    </div>
+			</div>
 
-<div class="page-header">
-    <div class="page-title">
-        <h1>
-            <i class="icon angle-left-icon back-link" onclick="history.length > 1 ? history.go(-1) : window.location = '{{ route('shop.home.index') }}';"></i>
-            {{ __('blog::app.blogs.title') }}
-        </h1>
-    </div>
-</div>
-
-@if(isset($posts) && count($posts) > 0)
-	<table>
-	@foreach($posts as $post)
-	<tr>
-		<td><a href="{{ route('blog-detail', [$post->slug]) }}">{{ $post->title }}</a></td>
-		<td><img src="{{ asset('/').$post->image }}" alt="{{ __('blog::app.blogs.title') }}" onerror="this.src='{{ asset('vendor/webkul/ui/assets/images/product/meduim-product-placeholder.png') }}'" ></td>
-		<td>{{ $post->content }}</td>
-	</tr>
-	@endforeach
-	</table>
-@endif
+			@if(isset($posts) && count($posts) > 0)
+				<div class="post-wrapper">
+						<ul class="row">
+							@foreach($posts as $post)
+									<li class="blog-post col-md-6 col-lg-4 col-xl-3">
+										<div class="content-wrap">
+											<div class="post-thumb-wrap">
+												<a href="{{ route('blog-detail', [$post->slug]) }}">
+													<img src="{{ asset('/').$post->image }}" alt="{{ __('blog::app.blogs.title') }}" onerror="this.src='{{ asset('vendor/webkul/ui/assets/images/product/meduim-product-placeholder.png') }}'">
+												</a>
+											</div>
+											<div class="content">
+												<div class="post-title">
+													<a href="{{ route('blog-detail', [$post->slug]) }}">{{ $post->title }}</a>
+												</div>
+												<div class="post-content"><p>{{ $post->content }}</p></div>
+											</div>
+											<div class="post-footer">
+												<div class="buttons">
+													<p>
+														<a href="{{ route('blog-detail', [$post->slug]) }}" class="btn btn-primary">
+														 Read More
+														</a>
+													</p>
+												</div>
+											</div>
+										</div>
+									</li>
+								
+							@endforeach
+						</ul>
+				</div>
+			@endif
+	</section>
 
 @endsection
