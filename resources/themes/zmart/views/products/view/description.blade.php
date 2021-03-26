12c9ce2e727bfb31a6ad32e4820d767f2effc96e
Kishan Parikh <kishan.strivedge@gmail.com>
2021-03-26 14:55:17 +0530

    Product Page Design Changes

--- resources/themes/zmart/views/products/view/description.blade.php
+++ resources/themes/zmart/views/products/view/description.blade.php
@@ -1,15 +1,15 @@
 {!! view_render_event('bagisto.shop.products.view.description.before', ['product' => $product]) !!}
 
-    <accordian :title="'{{ __('shop::app.products.description') }}'" :active="true">
+   <!--  <accordian :title="'{{ __('shop::app.products.description') }}'" :active="true">
         <div slot="header">
             <h3 class="no-margin display-inbl">
                 {{ __('velocity::app.products.details') }}
             </h3>
 
             <i class="rango-arrow"></i>
-        </div>
+        </div> -->
 
-        <div slot="body">
+        <!-- <div slot="body"> -->
             <div class="full-description">
                 {!! $product->description !!}
             </div>
@@ -78,7 +78,7 @@
             @endif
         </div>
             
-        </div>
-    </accordian>
+       <!--  </div>
+    </accordian> -->
 
 {!! view_render_event('bagisto.shop.products.view.description.after', ['product' => $product]) !!}
